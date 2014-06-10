40c3d90f73edd7bf70fe9eda1fc4708a8d188406
Rapha <Raphael@gocharm.com.tw>
2014-06-10 10:59:24 +0800

    tune login view & add fast train and .lang

--- www/themes/index/blocks/views/user/login/login.js
+++ www/themes/index/blocks/views/user/login/login.js
@@ -1,16 +1,23 @@
 ctrl.startup = function() {
-  ctrl.sel('#reg').hide();
   ctrl.sel('.regField').hide();
 };
 
-ctrl.reg = function() {
-  ctrl.sel('.form-signin').toggleClass('form-signin form-signup');
-  ctrl.sel('.form-signin-heading').toggleClass('form-signin-heading form-signup-heading');
-  ctrl.sel('#reg').show();
-  ctrl.sel('.regField').show();
-  ctrl.sel('.btn-group').hide();
+ctrl.switch = function(type) {
+  switch(type) {
+  case 'login':
+    ctrl.sel('.form-signup').toggleClass('form-signin form-signup');
+    ctrl.sel('.form-signup-heading').toggleClass('form-signin-heading form-signup-heading');
+    ctrl.sel('.regField').hide();
+    ctrl.sel('.loginField').show();
+    break;
+  case 'reg':
+    ctrl.sel('.form-signin').toggleClass('form-signin form-signup');
+    ctrl.sel('.form-signin-heading').toggleClass('form-signin-heading form-signup-heading');
+    ctrl.sel('.regField').show();
+    ctrl.sel('.loginField').hide();
+  }
 };
 
 function wrongInfo() {
-  ctrl.reg();
+  ctrl.switch('reg');
 }
