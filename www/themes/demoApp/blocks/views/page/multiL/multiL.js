3a4da4ba05596095cfa342c5693b6cf7b2bc3943
Ben Lue <ben.lue@gocharm.com.tw>
2014-07-11 22:08:10 +0800

    clean up directories and files

--- www/themes/demoApp/blocks/views/page/multiL/multiL.js
+++
@@ -1,5 +0,0 @@
-/* This function will be invoked when users make a language selection */
-ctrl.selectLang = function(sel) {
-	var  lang = $(sel).val();
-	window.location = '/page/multiL?_loc=' + lang;
-};
