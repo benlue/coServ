f6889a60442f74926cee85ed7d5376ec7883692e
Ben Lue <ben.lue@gocharm.com.tw>
2014-08-08 10:12:14 +0800

    bug fix and www path configurable

--- coServ.js
+++ coServ.js
@@ -1,14 +1,16 @@
 var  path = require('path'),
 	 config = require('./config.json');
 
-// change this to the proper path
-var  wwwPath = __dirname;
+// deal with the www path
+var  wwwPath = config.wwwPath;
+if (!wwwPath)
+	wwwPath = path.join(__dirname, './www/');
 if (path.sep === '\\')
 	wwwPath = wwwPath.replace(/\\/, '/');
 
 var connect = require('connect'),
     http = require('http'),
-    siteLookup = require('./lib/server/SiteLookup.js')(wwwPath + '/www/themes/'),
+    siteLookup = require('./lib/server/SiteLookup.js')(wwwPath + '/themes/'),
     uploader = require('./lib/server/Uploader.js')(config),
     webFront = require('./lib/server/WebFront.js')();
 
@@ -16,7 +18,7 @@ var app = connect()
 	.use(require('static-favicon')())
 	.use(require('morgan')('dev'))
 	.use(require('cookie-parser')())
-	.use(require('serve-static')(wwwPath + '/www/cont'))
+	.use(require('serve-static')(wwwPath + '/cont'))
 	//.use(require('express-session')({ secret: 'my secret here' }))
 	//.use(connect.static(wwwPath + '/www/cont'))
 	.use(siteLookup)
