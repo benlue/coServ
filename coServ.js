d63c12d66a30663b8cadb49d01cc2127f781959b
Ben Lue <ben.lue@gocharm.com.tw>
2016-01-27 10:19:36 +0800

    half working coServ.js

--- coServ.js
+++ coServ.js
@@ -1,21 +1,23 @@
 var  config = require('./lib/server/config.js'),
-	 bodyParser = require('body-parser');
+	 bodyParser = require('body-parser'),
+	 serverStatic = require('serve-static'),
+	 path = require('path'),
+	 connect = require('connect'),
+     http = require('http'),
+     siteLookup = require('./lib/server/SiteLookup.js'),
+     uploader = require('./lib/server/Uploader.js')(config.getApiEngine()),
+     webFront = require('./lib/server/WebFront.js')();
 
 // deal with the www path
 var  port = config.getServer().port || 8080,
 	 wwwPath = config.getWWW();
 
-var connect = require('connect'),
-    http = require('http'),
-    siteLookup = require('./lib/server/SiteLookup.js'),
-    uploader = require('./lib/server/Uploader.js')(config.getApiEngine()),
-    webFront = require('./lib/server/WebFront.js')();
-
 var app = connect()
-	//.use(require('serve-favicon')(wwwPath + 'cont/shared/favicon.ico'))
 	.use(require('morgan')('dev'))
 	.use(require('cookie-parser')())
-	.use(require('serve-static')(wwwPath + '/cont'))
+	.use(serverStatic(path.join(__dirname, './www/admin/cont')))
+	.use(serverStatic(wwwPath + '/cont'))
+	//.use(require('serve-static')(wwwPath + '/cont'))
 	//.use(require('express-session')({ secret: 'my secret here' }))
 	.use(siteLookup)
 	.use(uploader)
