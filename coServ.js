76c98f05b6a13441c93e4e0e9e8dc92dbd4b6b16
benlue <ben@Macde-MacBook-Pro.local>
2018-04-27 10:18:29 +0800

    allowed the config file to be set in the command line

--- coServ.js
+++ coServ.js
@@ -2,9 +2,19 @@
 * coServ
 * authors: Ben Lue
 * license: MIT
-* Copyright(c) 2016 Gocharm Inc.
+* Copyright(c) 2016 ~ 2018 Gocharm Inc.
 */
-var  coServ = require('./lib/server/main.js');
+const  fs = require('fs'),
+       coServ = require('./lib/server/main.js');
 
-coServ.init( require('./config.json') );
+let  config,
+     argv = process.argv;
+
+// allow the config file to be specified in the command line
+if (argv.length == 4 && argv[2] == '-c')
+    config = JSON.parse( fs.readFileSync( argv[3] ) );
+else
+    config = require('./config.json');
+
+coServ.init( config );
 coServ.restart();
